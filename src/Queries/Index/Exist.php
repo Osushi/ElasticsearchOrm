09158e7f88d19abffcc2acb32e53d8ca761abca9
Osushi <tdu.future.i@gmail.com>
2018-09-06 13:55:49 +0900

    Create index exists query

---
+++ src/Queries/Index/Exist.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Osushi\ElasticsearchOrm\Queries\Index;
+
+use Osushi\ElasticsearchOrm\Queries\Query;
+
+class Exist implements Query
+{
+    private $index;
+
+    public function index(
+        string $index
+    ) {
+        $this->index = $index;
+        return $this;
+    }
+
+    public function getIndex()
+    {
+        return $this->index;
+    }
+
+    public function build(): array
+    {
+        $params = [
+            'index' => $this->getIndex(),
+        ];
+
+        return $params;
+    }
+}
